c181de188fba0880392a4953167a58eb24b1c95a
Hassan Mccutchen <mccutchen.hassan@gmail.com>
2018-07-23 15:28:00 -0400

    changed

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var x = xhttp.responseText;
-      JSON.parse(x);
+
       console.log(x);
     }
   };
