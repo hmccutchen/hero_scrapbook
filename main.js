46676fe1da6f3899d3b20dc5e15d87d22d0e95f5
Hassan Mccutchen <mccutchen.hassan@gmail.com>
2018-07-23 15:56:16 -0400

    json added

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 
-  var xhttp = new XMLHttpRequest();
+var xhttp = new XMLHttpRequest();
 
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var x = xhttp.responseText;
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var x = xhttp.responseText;
+       var y = JSON.parse(x);
+    name = y.universe.heroes.name;
+  }
 
-      console.log(x);
-    }
-  };
 
-  xhttp.open("GET", "https://raw.githubusercontent.com/hmccutchen/hero_scrapbook/master/heroes.json", true);
-  xhttp.send();
+function createHero(name){
+
+name = name;
+
+  batman = {
+    name: name
+  }
+  console.log(name);
+}
+createHero(name);
+};
+
+xhttp.open("GET", "https://raw.githubusercontent.com/hmccutchen/hero_scrapbook/master/heroes.json", true);
+xhttp.send();
+
+
+
+
