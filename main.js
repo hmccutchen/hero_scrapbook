bbc2004b5f8d831a079dbf2ddf6de0ae438cd4ff
Hassan Mccutchen <mccutchen.hassan@gmail.com>
2018-07-23 15:10:51 -0400

    jsdjd

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 
   var xhttp = new XMLHttpRequest();
+
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     document.getElementById("demo").innerHTML = this.responseText;
+      var x = xhttp.responseText;
+      console.log(x.heroes.name);
+
     }
   };
-  xhttp.open("GET", "ajax_info.txt", true);
+
+  xhttp.open("GET", "https://raw.githubusercontent.com/hmccutchen/hero_scrapbook/master/heroes.json", true);
   xhttp.send();
